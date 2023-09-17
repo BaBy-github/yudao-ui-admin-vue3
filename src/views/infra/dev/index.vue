a0ea34cd29311c76ab26a773d0c66ebc28ff0f9f
BaBy-github <70896306+BaBy-github@users.noreply.github.com>
2023-09-17 17:51:14 +0800

    init

---
+++ src/views/infra/dev/index.vue
@@ -0,0 +1,13 @@
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'index'
+})
+</script>
+
+<template>
+  <json-schema-editor />
+</template>
+
+<style scoped lang="scss"></style>
