d411232d7085146d3e2071c47a20ddd7d77a8e64
BaBy-github <70896306+BaBy-github@users.noreply.github.com>
2023-10-29 15:05:00 +0800

    clear rename

--- src/components/bpmnProcessDesigner/package/penal/task/task-components/ServiceTask.vue
+++ src/components/bpmnProcessDesigner/package/penal/task/task-components/ServiceTask.vue
@@ -1,6 +1,6 @@
 <template>
   <div style="margin-top: 16px">
-    {{ fieldsListOfListener }}
+    {{ fieldsListOfServiceTask }}
     <el-form-item label="执行java类">
       <el-input
         v-model="serviceTaskForm.class"
@@ -35,12 +35,12 @@
     </el-form-item>
     <p class="listener-filed__title">
       <span><Icon icon="ep:menu" />注入字段：</span>
-      <el-button size="small" disabled type="primary" @click="openListenerFieldForm(null)"
+      <el-button size="small" disabled type="primary" @click="openServiceTaskFieldForm(null)"
         >添加字段</el-button
       >
     </p>
     <el-table
-      :data="fieldsListOfListener"
+      :data="fieldsListOfServiceTask"
       size="small"
       max-height="240"
       fit
@@ -67,7 +67,7 @@
             size="small"
             disabled
             link
-            @click="openListenerFieldForm(scope.row, scope.$index)"
+            @click="openServiceTaskFieldForm(scope.row, scope.$index)"
             >编辑</el-button
           >
           <el-divider direction="vertical" />
@@ -85,16 +85,16 @@
     <!-- 注入西段 编辑/创建 部分 -->
     <el-dialog
       title="字段配置"
-      v-model="listenerFieldFormModelVisible"
+      v-model="serviceTaskFieldFormModelVisible"
       width="600px"
       append-to-body
       destroy-on-close
     >
       <el-form
-        :model="listenerFieldForm"
+        :model="serviceTaskFieldForm"
         size="small"
         label-width="96px"
-        ref="listenerFieldFormRef"
+        ref="serviceTaskFieldFormRef"
         style="height: 136px"
       >
         <el-form-item
@@ -102,14 +102,14 @@
           prop="name"
           :rules="{ required: true, trigger: ['blur', 'change'] }"
         >
-          <el-input v-model="listenerFieldForm.name" clearable />
+          <el-input v-model="serviceTaskFieldForm.name" clearable />
         </el-form-item>
         <el-form-item
           label="字段类型："
           prop="fieldType"
           :rules="{ required: true, trigger: ['blur', 'change'] }"
         >
-          <el-select v-model="listenerFieldForm.fieldType">
+          <el-select v-model="serviceTaskFieldForm.fieldType">
             <el-option
               v-for="i in Object.keys(fieldTypeObject)"
               :key="i"
@@ -119,27 +119,27 @@
           </el-select>
         </el-form-item>
         <el-form-item
-          v-if="listenerFieldForm.fieldType === 'string'"
+          v-if="serviceTaskFieldForm.fieldType === 'string'"
           label="字段值："
           prop="stringValue"
           key="field-string"
           :rules="{ required: true, trigger: ['blur', 'change'] }"
         >
-          <el-input v-model="listenerFieldForm.stringValue" clearable />
+          <el-input v-model="serviceTaskFieldForm.stringValue" clearable />
         </el-form-item>
         <el-form-item
-          v-if="listenerFieldForm.fieldType === 'expression'"
+          v-if="serviceTaskFieldForm.fieldType === 'expression'"
           label="表达式："
           prop="expression"
           key="field-expression"
           :rules="{ required: true, trigger: ['blur', 'change'] }"
         >
-          <el-input v-model="listenerFieldForm.expression" clearable />
+          <el-input v-model="serviceTaskFieldForm.expression" clearable />
         </el-form-item>
       </el-form>
       <template #footer>
-        <el-button size="small" @click="listenerFieldFormModelVisible = false">取 消</el-button>
-        <el-button size="small" type="primary" @click="saveListenerFiled">确 定</el-button>
+        <el-button size="small" @click="serviceTaskFieldFormModelVisible = false">取 消</el-button>
+        <el-button size="small" type="primary" @click="saveServiceTaskField">确 定</el-button>
       </template>
     </el-dialog>
   </div>
@@ -169,39 +169,40 @@ const bpmnElement = ref()
 const bpmnInstances = () => (window as any)?.bpmnInstances
 
 // field
-const fieldsListOfListener = ref<any>([])
+const fieldsListOfServiceTask = ref<any>([])
 const fieldTypeObject = ref(fieldType)
-const listenerFieldFormRef = ref()
-const listenerFieldForm = ref<any>({}) // 监听器 注入字段 详情表单
-const editingListenerFieldIndex = ref(-1) // 字段所在下标，-1 为新增
-const listenerFieldFormModelVisible = ref(false) // 注入字段表单弹窗 显示状态
-const listenerForm = ref<any>({})
+const serviceTaskFieldFormRef = ref()
+const serviceTaskFieldForm = ref<any>({}) // Service Task 注入字段 详情表单
+const editingServiceTaskFieldIndex = ref(-1) // 字段所在下标，-1 为新增
+const serviceTaskFieldFormModelVisible = ref(false) // 注入字段表单弹窗 显示状态
 
 // 打开字段编辑弹窗
-const openListenerFieldForm = (field, index?) => {
-  listenerFieldForm.value = field ? JSON.parse(JSON.stringify(field)) : {}
-  editingListenerFieldIndex.value = field ? index : -1
-  listenerFieldFormModelVisible.value = true
+const openServiceTaskFieldForm = (field, index?) => {
+  serviceTaskFieldForm.value = field ? JSON.parse(JSON.stringify(field)) : {}
+  editingServiceTaskFieldIndex.value = field ? index : -1
+  serviceTaskFieldFormModelVisible.value = true
   nextTick(() => {
-    if (listenerFieldFormRef.value) listenerFieldFormRef.value.clearValidate()
+    if (serviceTaskFieldFormRef.value) serviceTaskFieldFormRef.value.clearValidate()
   })
 }
 // 保存java类注入字段
-const saveListenerFiled = async () => {
-  let validateStatus = await listenerFieldFormRef.value.validate()
+const saveServiceTaskField = async () => {
+  let validateStatus = await serviceTaskFieldFormRef.value.validate()
   if (!validateStatus) return // 验证不通过直接返回
-  if (editingListenerFieldIndex.value === -1) {
-    fieldsListOfListener.value.push(listenerFieldForm.value)
-    // listenerForm.value.fields.push(listenerFieldForm.value)
+  if (editingServiceTaskFieldIndex.value === -1) {
+    fieldsListOfServiceTask.value.push(serviceTaskFieldForm.value)
   } else {
-    fieldsListOfListener.value.splice(editingListenerFieldIndex.value, 1, listenerFieldForm.value)
-    // listenerForm.value.fields.splice(editingListenerFieldIndex.value, 1, listenerFieldForm.value)
+    fieldsListOfServiceTask.value.splice(
+      editingServiceTaskFieldIndex.value,
+      1,
+      serviceTaskFieldForm.value
+    )
   }
-  updateElementExtensions(bpmnElement.value, fieldsListOfListener.value)
+  updateElementExtensions(bpmnElement.value, fieldsListOfServiceTask.value)
   console.log(bpmnInstances().bpmnElement.businessObject.extensionElements)
-  listenerFieldFormModelVisible.value = false
+  serviceTaskFieldFormModelVisible.value = false
   nextTick(() => {
-    listenerFieldForm.value = {}
+    serviceTaskFieldForm.value = {}
   })
 }
 
@@ -214,7 +215,7 @@ const resetTaskForm = () => {
     bpmnElement.value?.businessObject.extensionElements.values.filter(
       (ex) => ex.$type === `${prefix}:Field`
     ) ?? []
-  fieldsListOfListener.value = _.map(fields, (field) => ({
+  fieldsListOfServiceTask.value = _.map(fields, (field) => ({
     ...field,
     fieldType: field.stringValue ? 'string' : 'expression'
   }))
