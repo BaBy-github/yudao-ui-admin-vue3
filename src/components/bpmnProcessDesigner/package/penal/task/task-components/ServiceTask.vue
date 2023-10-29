551994123c90e99d067b953ea3d2fb42e89a6fa9
BaBy-github <70896306+BaBy-github@users.noreply.github.com>
2023-10-29 12:30:27 +0800

    Service Task ready resetTaskForm

--- src/components/bpmnProcessDesigner/package/penal/task/task-components/ServiceTask.vue
+++ src/components/bpmnProcessDesigner/package/penal/task/task-components/ServiceTask.vue
@@ -1,5 +1,6 @@
 <template>
   <div style="margin-top: 16px">
+    {{ serviceTaskForm }}
     <el-form-item label="执行java类">
       <el-input
         v-model="serviceTaskForm.class"
@@ -32,10 +33,116 @@
         @change="updateElementTask()"
       />
     </el-form-item>
+    <p class="listener-filed__title">
+      <span><Icon icon="ep:menu" />注入字段：</span>
+      <el-button size="small" type="primary" @click="openListenerFieldForm(null)"
+        >添加字段</el-button
+      >
+    </p>
+    <el-table
+      :data="fieldsListOfListener"
+      size="small"
+      max-height="240"
+      fit
+      border
+      style="flex: none"
+    >
+      <el-table-column label="序号" width="50px" type="index" />
+      <el-table-column label="字段名称" min-width="100px" prop="name" />
+      <el-table-column
+        label="字段类型"
+        min-width="80px"
+        show-overflow-tooltip
+        :formatter="(row) => fieldTypeObject[row.fieldType]"
+      />
+      <el-table-column
+        label="字段值/表达式"
+        min-width="100px"
+        show-overflow-tooltip
+        :formatter="(row) => row.string || row.expression"
+      />
+      <el-table-column label="操作" width="100px">
+        <template #default="scope">
+          <el-button size="small" link @click="openListenerFieldForm(scope.row, scope.$index)"
+            >编辑</el-button
+          >
+          <el-divider direction="vertical" />
+          <!--          <el-button-->
+          <!--            size="small"-->
+          <!--            link-->
+          <!--            style="color: #ff4d4f"-->
+          <!--            @click="removeListenerField(scope.row, scope.$index)"-->
+          <!--            >移除</el-button-->
+          <!--          >-->
+        </template>
+      </el-table-column>
+    </el-table>
+    <!-- 注入西段 编辑/创建 部分 -->
+    <el-dialog
+      title="字段配置"
+      v-model="listenerFieldFormModelVisible"
+      width="600px"
+      append-to-body
+      destroy-on-close
+    >
+      <el-form
+        :model="listenerFieldForm"
+        size="small"
+        label-width="96px"
+        ref="listenerFieldFormRef"
+        style="height: 136px"
+      >
+        <el-form-item
+          label="字段名称："
+          prop="name"
+          :rules="{ required: true, trigger: ['blur', 'change'] }"
+        >
+          <el-input v-model="listenerFieldForm.name" clearable />
+        </el-form-item>
+        <el-form-item
+          label="字段类型："
+          prop="fieldType"
+          :rules="{ required: true, trigger: ['blur', 'change'] }"
+        >
+          <el-select v-model="listenerFieldForm.fieldType">
+            <el-option
+              v-for="i in Object.keys(fieldTypeObject)"
+              :key="i"
+              :label="fieldTypeObject[i]"
+              :value="i"
+            />
+          </el-select>
+        </el-form-item>
+        <el-form-item
+          v-if="listenerFieldForm.fieldType === 'string'"
+          label="字段值："
+          prop="string"
+          key="field-string"
+          :rules="{ required: true, trigger: ['blur', 'change'] }"
+        >
+          <el-input v-model="listenerFieldForm.string" clearable />
+        </el-form-item>
+        <el-form-item
+          v-if="listenerFieldForm.fieldType === 'expression'"
+          label="表达式："
+          prop="expression"
+          key="field-expression"
+          :rules="{ required: true, trigger: ['blur', 'change'] }"
+        >
+          <el-input v-model="listenerFieldForm.expression" clearable />
+        </el-form-item>
+      </el-form>
+      <template #footer>
+        <el-button size="small" @click="listenerFieldFormModelVisible = false">取 消</el-button>
+        <el-button size="small" type="primary" @click="saveListenerFiled">确 定</el-button>
+      </template>
+    </el-dialog>
   </div>
 </template>
 
 <script lang="ts" setup>
+import { fieldType } from '@/components/bpmnProcessDesigner/package/penal/listeners/utilSelf'
+
 defineOptions({ name: 'ScriptTask' })
 const props = defineProps({
   id: String,
@@ -51,6 +158,41 @@ const serviceTaskForm = ref<any>({})
 const bpmnElement = ref()
 
 const bpmnInstances = () => (window as any)?.bpmnInstances
+
+// field
+const fieldsListOfListener = ref<any>([])
+const fieldTypeObject = ref(fieldType)
+const listenerFieldFormRef = ref()
+const listenerFieldForm = ref<any>({}) // 监听器 注入字段 详情表单
+const editingListenerFieldIndex = ref(-1) // 字段所在下标，-1 为新增
+const listenerFieldFormModelVisible = ref(false) // 注入字段表单弹窗 显示状态
+const listenerForm = ref<any>({})
+
+// 打开字段编辑弹窗
+const openListenerFieldForm = (field, index?) => {
+  listenerFieldForm.value = field ? JSON.parse(JSON.stringify(field)) : {}
+  editingListenerFieldIndex.value = field ? index : -1
+  listenerFieldFormModelVisible.value = true
+  nextTick(() => {
+    if (listenerFieldFormRef.value) listenerFieldFormRef.value.clearValidate()
+  })
+}
+// 保存监听器注入字段
+const saveListenerFiled = async () => {
+  // let validateStatus = await listenerFieldFormRef.value.validate()
+  // if (!validateStatus) return // 验证不通过直接返回
+  // if (editingListenerFieldIndex.value === -1) {
+  //   fieldsListOfListener.value.push(listenerFieldForm.value)
+  //   // listenerForm.value.fields.push(listenerFieldForm.value)
+  // } else {
+  //   fieldsListOfListener.value.splice(editingListenerFieldIndex.value, 1, listenerFieldForm.value)
+  //   // listenerForm.value.fields.splice(editingListenerFieldIndex.value, 1, listenerFieldForm.value)
+  // }
+  // listenerFieldFormModelVisible.value = false
+  // nextTick(() => {
+  //   listenerFieldForm.value = {}
+  // })
+}
 
 const resetTaskForm = () => {
   for (let key in defaultTaskForm.value) {
